5d5127c4ed63bf2b944f44ff17caa136f87885c8
hossam-khalaf <hossamhassan303089@gmail.com>
2022-09-12 16:56:35 +0700

    first commit

---
+++ src/Register.js
@@ -0,0 +1,12 @@
+import { useRef, useState, useEffect } from 'react'
+import {
+	faCheck,
+	faTimes,
+	faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons'
+
+const Register = () => {
+	return <div>hello there!</div>
+}
+
+export default Register
